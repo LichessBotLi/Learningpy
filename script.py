0724a7be332057b7b480a06e5574b9ed902a0c59
LichessBotLi <102945249+LichessBotLi@users.noreply.github.com>
2025-07-24 22:06:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
 name = input("Enter your name: ")
 age = input("Enter your age baby: ")
 age = int(age)
-print("Hello my name is", name "and i will be " age + 1 "year old this time")
+print("Hello my name is", name, "and i will be " , age + 1 , "year old this time")
