2168a107ceaf1db3b74727a62f0791335ebba042
LichessBotLi <102945249+LichessBotLi@users.noreply.github.com>
2025-07-24 22:05:41 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,4 @@
-print("Utsa")
-print("Pizza")
-
-name="Utsa"
-food="pizza"
-print(name, "is that")
-print("My favourite food is",food)
-
-nu=1
-no=2
-sum= nu + no
-print(sum)
+name = input("Enter your name: ")
+age = input("Enter your age baby: ")
+age = int(age)
+print("Hello my name is", name "and i will be " age + 1 "year old this time")
